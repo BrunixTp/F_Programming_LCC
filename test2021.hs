e47fff7ee00288e686bd95ed272b870269a18b25
BrunixTp <a96544@alunos.uminho.pt>
2021-02-09 01:55:32 +0000

    stuff

--- test2021.hs
+++ test2021.hs
@@ -27,13 +27,13 @@ calculate = foldl (\(acc1,acc2) (a,b)-> (a:acc1, b+acc2)) ([],2)
 
 -- 3
 partes :: String -> Char -> [String]
-partes [] = [[]]
+partes [] _ = [[]]
 partes l a = partesAux l a []
 
 partesAux :: String -> Char -> String -> [String]
 partesAux [] _ acc = [acc]
 partesAux (h:t) n acc | h == n  = acc: partesAux t n []
-                      | otherwise = partesAux t n [acc ++ [x]]
+                      | otherwise = partesAux t n (acc ++ [h])
 
 
 -- 4
@@ -42,7 +42,7 @@ a1 =                           Node 5
             (Node 3 Empty Empty) (Node 7 Empty 
                                       (Node 9 Empty Empty)) 
 --a 
-minWMin :: Ord a => BTree a -> (a, BTree a) -- does a and b in one go
+minWMin :: Ord a => BTree a -> (a, BTree a)
 minWMin Empty = error "No Empty Trees"
 minWMin (Node n Empty _) = (n, Empty)
 minWMin (Node n e d) = (e1, Node n e2 d) 
@@ -82,5 +82,32 @@ type Nome = String
 fs1 = Dir "usr" [Dir "xxx" [File "abc.txt", File "readme", Dir "PF" [File "exemplo.hs"]], 
                  Dir "yyy" [], Dir "zzz" [Dir "tmp" [], File "teste.c"] ]
 
--- a 
---fichs :: FileSystem -> [Nome]
+-- a
+-- lists all files in a file System
+fichs :: FileSystem -> [Nome]
+fichs (File x) = [x]
+fichs (Dir x l) = concatMap fichs l
+
+-- b
+{-
+lists the names of files in a file System in a determined Path.
+If the path is invalid, the function returns Nothing.
+For example, dirFiles fs1 ["usr","xxx"] == Just ["abc.txt","readme"]
+-}
+dirFiles :: FileSystem -> [Nome] -> Maybe [Nome]
+dirFiles _ [] = Nothing
+dirFiles (Dir x l) (h:t) 
+
+
+          
+ --c
+{-
+reads a path e prints the file names in the directory
+indicated by the path. The path must be read as
+a string with the usual format "usr/xxx/PF".
+If the path isn't valid, then it must print "not a directory"
+-}
+--listFiles :: FileSystem -> IO ()
+
+
+
